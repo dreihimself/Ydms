ac5c46eeac745399bb6075a19fc1a029ac899ee6
Andrei <Andreiaspuria16@gmail.com>
2024-05-13 16:47:25 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        vue({
+            template: { transformAssetUrls }
+        }),
+
+          // @quasar/plugin-vite options list:
+          // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+        quasar({
+            sassVariables: 'resources/css/quasar-variables.sass'
+        })
+    ],
+});
