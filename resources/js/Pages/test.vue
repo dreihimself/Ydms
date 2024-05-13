b82c587220e85530e0d682723088a175a36f4daa
Andrei <Andreiaspuria16@gmail.com>
2024-05-14 07:53:29 +0800

    test header and sidebar

--- resources/js/Pages/test.vue
+++ resources/js/Pages/test.vue
@@ -1,20 +1,20 @@
 <template>
     <q-layout view="hHh lpR fFf">
 
-      <q-header elevated class="bg-primary text-white">
+      <q-header elevated class="bg-dark text-white">
         <q-toolbar>
           <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
           <q-toolbar-title>
             <q-avatar>
-              <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Sangguniang_Kabataan_logo.svg">
             </q-avatar>
            YOUTH DEVELOPMENT MANAGEMENT SYSTEM
           </q-toolbar-title>
         </q-toolbar>
       </q-header>
 
-      <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered class="bg-dark">
         <!-- drawer content -->
       </q-drawer>
 
