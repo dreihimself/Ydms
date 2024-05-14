06d1b14bdd244cdd9a590a7ab0492061ce819682
Jaynard A. Raqueño <jaynard.raqueno@student.dmmmsu.edu.ph>
2024-05-14 14:28:37 +0800

    Settings dropdown

--- resources/js/Pages/test.vue
+++ resources/js/Pages/test.vue
@@ -43,11 +43,25 @@
         class="q-ml-lg text-h6" 
       >
         <q-card class="bg-grey-9">
-          <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
-          </q-card-section>
+          <q-list bordered separator>
+      <q-item clickable v-ripple>
+        <q-item-section>Single line item</q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label>Item with caption</q-item-label>
+          <q-item-label caption>Caption</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label overline>OVERLINE</q-item-label>
+          <q-item-label>Item with caption</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
         </q-card>
       </q-expansion-item>
 
