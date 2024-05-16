3c90898a809f2d0fc87a279712a95ac065a51b03
Jaynard A. Raqueño <jaynard.raqueno@student.dmmmsu.edu.ph>
2024-05-16 09:24:26 +0800

    layout

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -3,10 +3,8 @@
         <q-header elevated class="bg-dark text-white" >
             <q-toolbar style="height:100px">
                 <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-                <q-toolbar-title>
-                    <q-avatar>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Sangguniang_Kabataan_logo.svg">
-                    </q-avatar>
+                <q-toolbar-title style="align-items: center; display: flex; ">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Sangguniang_Kabataan_logo.svg" style="height: 50px;" class="q-mr-sm">
                     YOUTH DEVELOPMENT MANAGEMENT SYSTEM
                 </q-toolbar-title>
             </q-toolbar>
@@ -46,54 +44,47 @@
                                     </q-item-section>
                                 </q-item>
                             </Navigation>
-                            <Navigation class="m-ml-none" href="">
-                                <q-item class="q-ma-xm navigation-item text-white q-py-none" :class="$page.component === '' ? 'bg-grey-14' : null">
-                                    <q-item clickable v-ripple @click="showGenerateReports = !showGenerateReports" class="q-px-none">
-                                        <q-item-section>
-                                            <q-item-label>Generate Reports</q-item-label>
-                                        </q-item-section>
-                                        </q-item>
-                                        <q-slide-transition>
-                                        <div v-if="showGenerateReports" class="q-pa-md">
-                                            <p>KK Profiling Summary</p>
-                                            <p>SK Council Activity</p>
-                                            <p>Programs Summary</p>
-                                            <p>Activity Summary</p>
-                                        </div>
-                                        </q-slide-transition>
-                                        </q-item>
+                                <q-expansion-item 
+    expand-separator
+    label="Generate Reports"
+    :default-opened="$page.component.startsWith('reports')" 
+>
+  <q-list>
+    <q-item clickable to="/kkprofilingsummary" :active="$page.component === 'kkprofilingsummary'">
+      <q-item-section>KK Profiling Summary</q-item-section>
+    </q-item>
+    <q-item clickable to="/skcouncilactivity" :active="$page.component === 'skcouncilactivity'">
+      <q-item-section>SK Council Activity</q-item-section>
+    </q-item>
+    <q-item clickable to="/programssummary" :active="$page.component === 'programssummary'">
+      <q-item-section>Programs Summary</q-item-section>
+    </q-item>
+    <q-item clickable to="/activitysummary" :active="$page.component === 'activitysummary'">
+      <q-item-section>Activity Summary</q-item-section>
+    </q-item>
+  </q-list>
+</q-expansion-item>
 
-                            </Navigation>
-                            <Navigation  href="">
-                                <q-item class="q-ma-xm navigation-item text-white q-py-none" :class="$page.component === '' ? 'bg-grey-14' : null">
-                                    <q-item clickable v-ripple @click="showContent = !showContent" class="q-px-none">
-                                        <q-item-section >
-                                            <q-item-label >Settings</q-item-label>
-                                        </q-item-section>
-                                        </q-item>
-                                        <q-slide-transition>
-                                        <div v-if="showContent" class="q-pa-md">
-                                            <Navigation href="usersettings">
-                                                <q-item class="q-ma-xm navigation-item text-white" :class="$page.component === 'usersettings' ? 'bg-grey-14' : null">
-                                                    <q-item-section>
-                                                        User Settings
-                                                    </q-item-section>
-                                                </q-item>
-                                            </Navigation>
-                                            <p>Dropdown Settings</p>
-                                            <Navigation href="SkOfficials">
-                                                <q-item class="q-ma-xm navigation-item text-white" :class="$page.component === 'SkOfficials' ? 'bg-grey-14' : null">
-                                                    <q-item-section>
-                                                        Sk Officials
-                                                    </q-item-section>
-                                                </q-item>
-                                            </Navigation>
-                                            <p>Barangay Registry</p>
-                                            <p>Department Head</p>
-                                        </div>
-                                        </q-slide-transition>
-                                </q-item>
-                            </Navigation>
+                                        <q-expansion-item 
+    expand-separator 
+    label="Settings"
+    :default-opened="$page.component.startsWith('settings')"  
+  >
+    <q-list>
+      <q-item clickable to="/usersettings" :active="$page.component === 'usersettings'">
+        <q-item-section>User Settings</q-item-section>
+      </q-item>
+      <q-item clickable to="/skofficials" :active="$page.component === 'skofficials'">
+        <q-item-section>Sk Officials</q-item-section>
+      </q-item>
+      <q-item clickable to="/barangayregistry" :active="$page.component === 'barangayregistry'">
+        <q-item-section>Barangay Registry</q-item-section>
+      </q-item>
+      <q-item clickable to="/departmenthead" :active="$page.component === 'departmenthead'">
+        <q-item-section>Department Head</q-item-section>
+      </q-item>
+    </q-list>
+  </q-expansion-item>
                             <Navigation href="/preference">
                                 <q-item class="q-ma-xm navigation-item text-white" :class="$page.component === 'PreferencePage' ? 'bg-grey-14' : null">
                                     <q-item-section>
