274fdf3a4b02846b1dea0f132cca498733d2b367
Andrei <Andreiaspuria16@gmail.com>
2024-05-16 13:36:34 +0800

    sk officials page minor changes

--- resources/js/Pages/SkOfficials.vue
+++ resources/js/Pages/SkOfficials.vue
@@ -5,7 +5,7 @@
             <!--tab here-->
             <div class="q-pa-md" style="margin-left: -45px">
 
-      <div class="q-gutter-y-md bg-primary" style="width: 370px; margin-left: 30px;">
+      <div class="q-gutter-y-md bg-primary" style="width: 20%; margin-left: 30px;">
 <q-tabs v-model="tab">
 <q-tab name="mails" class="q-tab q-pa-none q-pl-none q-mx-none">
   <div class="text-white text-bold" style="width: 100%; text-align: left">
@@ -31,8 +31,8 @@
                <div class=""> <q-form @submit="saveSettings" class="q-gutter-md"  style="width:100%">
               <q-input outlined v-model="name" dense label="Name" disable/>
               <div class="input-container">
-                <q-input v-model="date" outlined dense type="date" label="Birth Date" class="q-py-none" disable/>
-                <q-input outlined v-model="Age" label="Age" dense disable/>
+                <q-input v-model="date" outlined dense type="date" label="Birth Date" class="q-py-none" disable style="width:100%"/>
+                <q-input outlined v-model="Age" label="Age" dense disable style="width:100%"/>
               </div>
               <q-input outlined v-model="sex" dense label="Sex" disable/>
               <q-input outlined v-model="Barangay" dense label="Barangay" disable/>
@@ -74,7 +74,7 @@
                  flat bordered
                   :rows="rows"
                   :columns="columns"
-                  style="max-height: 100%; overflow-y: auto; thead{background-color: #1976D2;}"
+                  style="max-height: 100%; overflow-y: auto;"
                 />
               </div>
             </div>
@@ -108,21 +108,31 @@
 
 <script>
 const columns = [
-  {
-    align: 'left',
-    sortable: false,
-    headerStyle: {
+
+  { name: 'Position',  label: 'Position', field: 'Position', align:'left' ,headerStyle: {
             background: '#1976D2',
             color: 'white'
-          }
-
-  },
-  { name: 'Position',  label: 'Position', field: 'Position', align:'left'},
-  { name: 'name', label: 'name', field: 'name' },
-  { name: 'Sex', label: 'Sex', field: 'Sex' },
-  { name: 'mobile', label: 'mobile number', field: 'mobile' },
-  { name: 'email', label: 'email ', field: 'email' , align:'center'},
-  { name: 'action', label: 'action ', field: 'action' },
+          }},
+  { name: 'name', label: 'name', field: 'name',headerStyle: {
+            background: '#1976D2',
+            color: 'white'
+          } },
+  { name: 'Sex', label: 'Sex', field: 'Sex' ,headerStyle: {
+            background: '#1976D2',
+            color: 'white'
+          }},
+  { name: 'mobile', label: 'mobile number', field: 'mobile',headerStyle: {
+            background: '#1976D2',
+            color: 'white'
+          } },
+  { name: 'email', label: 'email ', field: 'email' , align:'center',headerStyle: {
+            background: '#1976D2',
+            color: 'white'
+          }},
+  { name: 'action', label: 'action ', field: 'action' ,headerStyle: {
+            background: '#1976D2',
+            color: 'white'
+          }},
 
 ]
 const rows = [
